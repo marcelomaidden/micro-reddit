44443768d9097b1de7d2f897c320ab9e4fed63e6
mke2111 <roymke134@gmail.com>
2020-11-13 01:00:37 +0300

    refactor comment model as requested

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :description, presence: true
-  validates :description, length: { in: 2..500 }
+  validates :description, presence: true, length: { in: 2..500 }
 end
